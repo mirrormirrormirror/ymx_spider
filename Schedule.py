a1e2435e2add2bdeccbcbb8f61c2d99d68fae1c5
mirror <guojing@imagedt.com>
2019-11-18 21:54:58 +0800

    编码问题

--- Schedule.py
+++ Schedule.py
@@ -8,30 +8,30 @@ if __name__ == '__main__':
     while True:
         keywordDao = KeywordDao()
 
-        print(u"进入关键词调度逻辑------")
+        print("into keyword logic")
         isLowLevelKeywordForRids = keywordDao.isLowLevelKeywordForRids()
         if isLowLevelKeywordForRids:
-            print(u'redis中的seed_keyword关键词个数低于阈值！put mysql data to redis..')
+            print('redis seed_keyword  low put mysql data to redis..')
             keywordDao.putKeywordToRedis()
         else:
-            print(u'redis中的seed_keyword关键词个数正常')
+            print('redis seed_keyword normal')
 
         keywordDao.close()
 
         detailLinkDao = DetailLinkDao()
         isLowLevelDetailLinksForRedis = detailLinkDao.isLowLevelDetailLinksForRedis()
         if isLowLevelDetailLinksForRedis:
-            print(u'redis中的seed_detail_link连接个数低于阈值！put mysql data to redis..')
+            print('redis seed_detail_link low put mysql data to redis..')
             detailLinkDao.putDetailLinksToRedis()
         else:
-            print(u'redis中的seed_detail_link连接个数正常')
+            print('redis seed_detail_link normal')
         detailLinkDao.close()
 
         dirDetailLinkDao = DirDetailLinkDao()
         if dirDetailLinkDao.isLowLevelDetailLinksForRedis():
-            print(u'redis中的seed_dir_detail_link连接个数低于阈值！put mysql data to redis..')
+            print(u'redis中的seed_dir_detail_link low put mysql data to redis..')
             dirDetailLinkDao.putDetailLinksToRedis()
         else:
-            print(u'redis中的seed_dir_detail_link连接个数正常')
+            print(u'redis中的seed_dir_detail_link normal')
 
         time.sleep(5)
