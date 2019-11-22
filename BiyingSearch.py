9a0d6810f0a99084dff0e6b6f85f2370814f1fdb
mirror <guojing@imagedt.com>
2019-11-22 21:35:32 +0800

    搜索必应

--- BiyingSearch.py
+++ BiyingSearch.py
@@ -4,6 +4,7 @@ from KeywordDao import KeywordDao
 from DetailLinkDao import DetailLinkDao
 import pymysql
 import redis
+from selenium import webdriver
 from SlpLinkDao import SlpLinkDao
 import time
 
@@ -11,8 +12,8 @@ import time
 class SearchBiying:
 
     def __init__(self):
-        print('google init')
-        self.baseSearchUrl = 'https://cn.bing.com/search?q=site: amazon.com /slp/ %s currently unavailable&go=Search&qs=ds&first=%s&FORM=PERE2'
+        # print('google init')
+        # self.baseSearchUrl = 'https://cn.bing.com/search?q=site: amazon.com /slp/ %s currently unavailable&go=Search&qs=ds&first=%s&FORM=PERE2'
         self.keywordDao = KeywordDao()
         print('init keywordDao finish')
         self.detailLinkDao = DetailLinkDao()
@@ -20,8 +21,17 @@ class SearchBiying:
         self.slpLinkDao = SlpLinkDao()
         print('init slpLinkDao finish')
 
-        self.downloadCount = 0
-        self.downloadCountPoint = 0
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('window-size=1920x3000')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--hide-scrollbars')
+        chrome_options.add_argument('blink-settings=imagesEnabled=false')
+        chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--proxy-server=%s' % 'https://127.0.0.1:8388')
+        # chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.driver.get('https://cn.bing.com/?scope=web&FORM=QBRE')
 
         self.myRedis = redis.Redis(host='localhost', port=6379, db=0)
         self.googleHost = 'google_host'
@@ -65,7 +75,7 @@ class SearchBiying:
             self.myRedis.sadd(self.googleHost, str(row[0]))
 
     def isLastPage(self, text):
-        if '404 Charity' in text:
+        if 'sw_next' in text:
             return False
         else:
             return True
@@ -74,10 +84,7 @@ class SearchBiying:
         print('run---')
         keyword = keywordId2Keyword[1]
         keywordId = keywordId2Keyword[0]
-        # 获取第一页
-        url = self.getDownloadLink(keyword, 0)
-        print('one page link:' + url)
-        text = self.download(url)
+        text = self.sentKey(keyword)
         pageLinks = self.parsePageLink(text)
         keyword2slpLink = self.getKeyword2link(keywordId, pageLinks)
         self.keywordDao.updateKeywordState(keywordId, 2)
@@ -85,24 +92,36 @@ class SearchBiying:
         isLastPage = self.isLastPage(text)
         print('one page isLastPage:' + str(isLastPage))
 
-        pageNum = 1
         while not isLastPage:
-            nextPage = self.getDownloadLink(keyword, pageNum)
-            print('next page link:' + nextPage)
+            nextPage = self.clikNext()
             nextPageLinks = self.parsePageLink(nextPage)
-            print('next page link:' + str(nextPageLinks))
+            # print('next page link:' + str(nextPageLinks))
             keywordId2nextPageLink = self.getKeyword2link(keywordId, nextPageLinks)
             self.slpLinkDao.batchInsert(keywordId2nextPageLink)
             self.keywordDao.updateKeywordState(keywordId, 2)
             isLastPage = self.isLastPage(text)
             print('next page isLastPage:' + str(isLastPage))
-            if not isLastPage:
-                pageNum = pageNum + 1
+
 
     def close(self):
         self.keywordDao.close()
         self.detailLinkDao.close()
         self.myRedis.close()
+        self.driver.close()
+        self.driver.quit()
+
+    def sentKey(self, keyword):
+        print('sent key')
+        self.driver.find_element_by_css_selector(keyword)
+        self.driver.find_element_by_css_selector('#sb_form_go').click()
+        text = self.driver.page_source
+        return text
+
+    def clikNext(self):
+        print('clikNext')
+        self.driver.find_element_by_css_selector('li .sw_next').click()
+        text = self.driver.page_source
+        return text
 
 
 if __name__ == '__main__':
@@ -110,6 +129,7 @@ if __name__ == '__main__':
     while True:
         detailLinkDao = DetailLinkDao()
         keyWordDao = KeywordDao()
+        searchBiying = SearchBiying()
         try:
             id2keyword = keyWordDao.popKeyWordForRedis()
             print(id2keyword)
@@ -127,11 +147,12 @@ if __name__ == '__main__':
                 print(keyWord)
                 keywordId = id2keywordDic[0]
                 print(keywordId)
-                googleSearch = SearchBiying()
-                googleSearch.run(id2keywordDic)
+                searchBiying.run(id2keywordDic)
             keyWordDao.close()
             detailLinkDao.close()
+            searchBiying.close()
 
         except:
             keyWordDao.close()
             detailLinkDao.close()
+            searchBiying.close()
