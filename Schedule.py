090a86ab9e4aabeb033c93099c01484ce6a90621
mirror <guojing@imagedt.com>
2019-11-27 00:13:59 +0800

    slave

--- Schedule.py
+++ Schedule.py
@@ -7,40 +7,45 @@ from SlpLinkDao import SlpLinkDao
 
 if __name__ == '__main__':
     while True:
-        keywordDao = KeywordDao()
+        try:
+            keywordDao = KeywordDao()
 
-        print("into keyword logic")
-        isLowLevelKeywordForRids = keywordDao.isLowLevelKeywordForRids()
-        if isLowLevelKeywordForRids:
-            print('redis seed_keyword  low put mysql data to redis..')
-            keywordDao.putKeywordToRedis()
-        else:
-            print('redis seed_keyword normal')
+            print("into keyword logic")
+            isLowLevelKeywordForRids = keywordDao.isLowLevelKeywordForRids()
+            if isLowLevelKeywordForRids:
+                print('redis seed_keyword  low put mysql data to redis..')
+                keywordDao.putKeywordToRedis()
+            else:
+                print('redis seed_keyword normal')
 
-        keywordDao.close()
+            keywordDao.close()
 
-        detailLinkDao = DetailLinkDao()
-        isLowLevelDetailLinksForRedis = detailLinkDao.isLowLevelDetailLinksForRedis()
-        if isLowLevelDetailLinksForRedis:
-            print('redis seed_detail_link low put mysql data to redis..')
-            detailLinkDao.putDetailLinksToRedis()
-        else:
-            print('redis seed_detail_link normal')
-        detailLinkDao.close()
+            detailLinkDao = DetailLinkDao()
+            isLowLevelDetailLinksForRedis = detailLinkDao.isLowLevelDetailLinksForRedis()
+            if isLowLevelDetailLinksForRedis:
+                print('redis seed_detail_link low put mysql data to redis..')
+                detailLinkDao.putDetailLinksToRedis()
+            else:
+                print('redis seed_detail_link normal')
+            detailLinkDao.close()
 
-        dirDetailLinkDao = DirDetailLinkDao()
-        if dirDetailLinkDao.isLowLevelDetailLinksForRedis():
-            print('redis seed_dir_detail_link low put mysql data to redis..')
-            dirDetailLinkDao.putDetailLinksToRedis()
-        else:
-            print('redis seed_dir_detail_link normal')
-        dirDetailLinkDao.close()
+            dirDetailLinkDao = DirDetailLinkDao()
+            if dirDetailLinkDao.isLowLevelDetailLinksForRedis():
+                print('redis seed_dir_detail_link low put mysql data to redis..')
+                dirDetailLinkDao.putDetailLinksToRedis()
+            else:
+                print('redis seed_dir_detail_link normal')
+            dirDetailLinkDao.close()
 
 
-        slpLinkDao = SlpLinkDao()
-        isLowLevelDetailLinksForRedis = slpLinkDao.isLowLevelDetailLinksForRedis()
-        if isLowLevelDetailLinksForRedis:
-            print('low slp')
-            slpLinkDao.putSlpLinksToRedis()
-        slpLinkDao.close()
-        time.sleep(3)
+            slpLinkDao = SlpLinkDao()
+            isLowLevelDetailLinksForRedis = slpLinkDao.isLowLevelDetailLinksForRedis()
+            if isLowLevelDetailLinksForRedis:
+                print('low slp')
+                slpLinkDao.putSlpLinksToRedis()
+            slpLinkDao.close()
+            time.sleep(3)
+        except Exception as e:
+            print(e)
+        finally:
+            time.sleep(3)
