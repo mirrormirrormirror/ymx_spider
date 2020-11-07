ddf875bff3b03a50930960640430008f61b88201
mirror <guojing@imagedt.com>
2020-11-07 18:30:51 +0800

    改变搜索策略+try

--- test9.py
+++
@@ -1,5 +0,0 @@
-import requests
-import time
-# proxies = { "http": "http://170.106.3.150:8388", "https": "https://170.106.3.150:8388", }
-# page = requests.get('https://cn.bing.com/search?q=%E7%88%AC%E8%99%AB&qs=n&FORM=BESBTB&sp=-1&pq=%E7%88%AC%E8%99%AB&sc=0-2')
-# print(page.text)
